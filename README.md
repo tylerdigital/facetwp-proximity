91842445684c2976aca3ead55f75c3b7c2954506
mgibbs189 <mgibbs189@gmail.com>
2014-02-24 22:09:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+FacetWP - Proximity Facet
+=======================
+
+A [FacetWP](https://facetwp.com/) facet for finding posts by distance
+
+## Installation
+* Click the "Download ZIP" button on this page.
+* Unzip the folder and rename it to "facetwp-proximity"
+* Upload the folder into the /wp-content/plugins/ directory
+* Activate the plugin (FacetWP must also be active)
---
+++ facetwp-proximity.php
@@ -0,0 +1,51 @@
+<?php
+/*
+Plugin Name: FacetWP - Proximity
+Plugin URI: https://facetwp.com/
+Description: A FacetWP facet to filter posts by proximity
+Version: 1.0.0
+Author: Matt Gibbs
+Author URI: https://facetwp.com/
+GitHub Plugin URI: https://github.com/mgibbs189/facetwp-proximity
+GitHub Branch: 1.0.0
+
+Copyright 2014 Matt Gibbs
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/>.
+*/
+
+// exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+
+class FWP_Proximity
+{
+    public $lang;
+
+
+    function __construct() {
+        add_action( 'init' , array( $this, 'init' ) );
+    }
+
+
+    /**
+     * Intialize
+     */
+    function init() {
+
+    }
+}
+
+
+$fwp_proximity = new FWP_Proximity();
